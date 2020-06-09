3a7cf85acb0d46712a875394f961537d4436b9a2
bystritskiy123 <gor_ell@mail.ru>
2020-06-09 16:48:50 +0300

    v1.4

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <Header />
     <Aboutass />
+    <Card />
   </div>
 </template>
 
 <script>
   import Header from './components/Header.vue'
   import Aboutass from './components/Aboutass.vue'
+  import Card from './components/Card.vue'
 
   export default {
     name: 'App',
     components: {
       Header,
-      Aboutass
+      Aboutass,
+      Card
     }
   }
 </script>
---
+++ src/components/Card.vue
@@ -0,0 +1,84 @@
+<template>
+    <div id="card">
+        <div class="card-group">
+            <div class="card">
+                <img src="../assets/game.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h3 align="center">app development</h3>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis
+                        molestie dignissim. Nulla pellentesque et nisi et euismod. Pellentesque quis tristique magna.
+                    </p>
+
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis molestie dignissim.
+                        Nulla
+                        pellentesque et nisi et euismod. Pellentesque quis tristique magna.
+                    </p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../assets/game.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h3 align="center">game dasing</h3>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit. Nulla facilisis
+                        molestie dignissim. Nulla pellentesque et nisi et euismod. Pellentesque quis tristique magna.
+                    </p>
+
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis molestie dignissim.
+                        Nulla
+                        pellentesque et nisi et euismod. Pellentesque quis tristique magna.
+                    </p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../assets/game.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h3 align="center">content creation</h3>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis
+                        molestie dignissim. Nulla pellentesque et nisi et euismod. Pellentesque quis tristique magna.
+                    </p>
+
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis molestie dignissim.
+                        Nulla
+                        pellentesque et nisi et euismod. Pellentesque quis tristique magna.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Card',
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @font-face {
+        font-family: 'MyriadProRegular';
+        src: url('../fonts/MyriadProRegular.ttf') format("truetype");
+    }
+
+    #card {
+        margin-top: 15%;
+    }
+
+    .card {
+        border: 0px;
+        font-family: 'MyriadProRegular';
+    }
+
+    .card-img-top {
+        width: 20%;
+        margin-left: 40%;
+    }
+</style>
